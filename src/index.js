cf283147851fc88146afa352cfe84f07d157430c
Kamil <tomala.kamil@gmail.com>
2019-11-21 16:10:44 +0100

    Register a user

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ const cors = require("cors");
 const { verify } = require("jsonwebtoken");
 const { hash, compare } = require("bcryptjs");
 
-// 1. Register a user
 // 2. Login a user
 // 3. Logout a user
 // 4. Setup a protected route
@@ -30,3 +29,15 @@ server.use(express.urlencoded({ extended: true }));
 server.listen(process.env.PORT, () =>
   console.log(`Server listenig on port ${process.env.PORT}`)
 );
+
+// 1. Register a user
+server.post("/register", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    // 1. Check if user exist
+
+    const hashedPassword = await hash(password, 10);
+    console.log(hashedPassword);
+  } catch (err) {}
+});
