47806abe553f0d024e273a51575a2ff9eccc6f25
Kamil <tomala.kamil@gmail.com>
2019-11-21 15:46:13 +0100

    configuration express server

--- src/index.js
+++ src/index.js
@@ -4,3 +4,25 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const { verify } = require("jsonwebtoken");
 const { hash, compare } = require("bcryptjs");
+
+// 1. Register a user
+// 2. Login a user
+// 3. Logout a user
+// 4. Setup a protected route
+// 5. Get a new accestoken with a refresh token
+
+const server = express();
+
+// Use express middleware for easier cookie handling
+server.use(cookieParser());
+
+server.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true
+  })
+);
+
+// Needed to be able to read body data
+server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
