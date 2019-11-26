5280fd33fe2f6ec9d391860d547d7d67d6e209e6
Kamil <tomala.kamil@gmail.com>
2019-11-26 14:42:44 +0100

    Logged user

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Router, navigate } from "@reach/router";
+
+import Content from "./components/Content";
+import Login from "./components/Login";
+import Navigation from "./components/Navigation";
+import Protected from "./components/Protected";
+import Register from "./components/Register";
+
+export const UserContext = React.createContext([]);
 
 function App() {
-  return <div className="App">App</div>;
+  const [user, setUser] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  const logOutCallback = async () => {};
+
+  useEffect(() => {}, []);
+
+  return (
+    <UserContext.Provider value={[user, setUser]}>
+      <div className="App">
+        <Navigation logOutCallback={logOutCallback} />
+        <Router id="router">
+          <Login path="login" />
+          <Register path="register" />
+          <Protected path="protected" />
+          <Content path="/" />
+        </Router>
+      </div>
+    </UserContext.Provider>
+  );
 }
 
 export default App;
